ea8f56e225a07d16e20d861dbc6d09b5fb611840
MarieAboa25 <m.aboa@itsolivetti.it>
2025-12-10 17:40:12 +0100

    creazione environment ,  getter e setter

---
+++ environement__es6/src/environement__es6.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class environement__es6{
+
+    private int id;
+    private double temperature;
+    private double pressure;
+    private double humidity;
+    private double windSpeed;
+    private double windDirection;
+
+    // getter & setter
+    public void setTemperature(final double temperature)
+    {
+        this.temperature = temperature;
+
+    }
+    public void setPressure(final double pressure)
+    {
+        this.pressure = pressure;
+
+    }
+    public void setHumidity(final double humidity)
+    {
+        this.humidity = humidity;
+
+    }
+    public void setWindSpeed(final double windSpeed)
+    {
+        this.windSpeed = windSpeed;
+
+    }
+    public void setWindDirection(final double windDirection)
+    {
+        this.windDirection = windDirection;
+
+    }
+
+
+}
